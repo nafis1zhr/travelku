0d07c829cdaac934e6c997348d02469750223e46
nafis1zhr <162326557+nafis1zhr@users.noreply.github.com>
2024-05-17 08:53:13 +0700

    book now

--- lib/pages/book_now.dart
+++ lib/pages/book_now.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:travelku_app/pages/book_now.dart';
 import 'package:travelku_app/pages/discover.dart';
 import 'package:travelku_app/pages/on_boarding.dart';
 
